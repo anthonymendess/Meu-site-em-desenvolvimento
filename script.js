017c85a662cb618e13df5c7510193569fa05af6a
guilph12 <guilph120@gmail.com>
2023-01-18 16:47:34 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+window.sr = ScrollReveal({ reset: true });
+
+sr.reveal('.store-pulsar', {duration: 1000});
+
+sr.reveal('.search-box', {duration: 1000});
+
+window.sr = ScrollReveal({ reset: true });
+ sr.reveal('.container-1', {duration: 1000});
+
+// sr.reveal("search-box");     
